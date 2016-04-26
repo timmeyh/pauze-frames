622b6a7286f599f7ad035ff2fc0109240503ad69
root <root@SBEWS01238.simac.be>
2016-04-26 11:20:13 +0200

    initial commit with pauze.py test script

---
+++ pauze.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+
+while(1):
+	data = '\x00\x01\xff\xfa' + '\xff' * 42 + '\xC2\x27\x97\x7B'
+	pkt = Ether(src = "88: 51: fb: fe: 82: b3", dst = "01: 80: c2: 00: 00: 01", type = 0x8808 ) / data
+	sendp(pkt, iface = "eth0")
+
+
+
+
+#ANDY LAPTOP ADRES:             88:51:fb:fe:82:b3
+#multicast pauze frame mac:     01-80-C2-00-00-01 01: 80: c2: 00: 00: 01
+
+
+
